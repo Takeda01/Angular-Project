2194c006edfd57c30e2143a61ce0df57162809fd
Stanislav Dimitrov <stasikn@abv.bg>
2023-07-14 23:08:22 +0300

    initial commit

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactsComponent } from '../static/contacts/contacts.component';
 import { AboutComponent } from '../static/about/about.component';
+import { SupportComponent } from '../static/support/support.component';
+import { TermsComponent } from '../static/terms/terms.component';
 
 
 
 const routes: Routes = [
     {path: 'contacts', component: ContactsComponent},
-    {path: 'about', component: AboutComponent}
+    {path: 'about', component: AboutComponent},
+    {path: 'support', component: SupportComponent},
+    {path: 'terms&conditions', component: TermsComponent}
   
   
 ];
--- src/app/static/static.module.ts
+++ src/app/static/static.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactsComponent } from './contacts/contacts.component';
 import { AboutComponent } from './about/about.component';
+import { SupportComponent } from './support/support.component';
+import { RouterLink } from '@angular/router';
+import { TermsComponent } from './terms/terms.component';
 
 
 
 @NgModule({
   declarations: [
     ContactsComponent,
-    AboutComponent
+    AboutComponent,
+    SupportComponent,
+    TermsComponent
   ],
   imports: [
     CommonModule,
+    RouterLink
    
   ],
   exports: [
